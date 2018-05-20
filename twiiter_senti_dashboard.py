90594c77920edc05cce5bf2b839f72dece239a05
Sagar Patil <sagar010797@gmail.com>
2018-05-20 20:18:08 +0530

    done!

--- twiiter_senti_dashboard.py
+++ twiiter_senti_dashboard.py
@@ -1,18 +1,27 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
-from tweepy import StreamListener
+from tweepy.streaming import StreamListener
 import json
 import sqlite3
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from unidecode import unidecode
-
+import time
 
 analyzer = SentimentIntensityAnalyzer()
-
 
 conn = sqlite3.connect('twitter.db')
 c = conn.cursor()
 
+def create_table():
+    try:
+        c.execute("CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment REAL)")
+        c.execute("CREATE INDEX fast_unix ON sentiment(unix)")
+        c.execute("CREATE INDEX fast_tweet ON sentiment(tweet)")
+        c.execute("CREATE INDEX fast_sentiment ON sentiment(sentiment)")
+        conn.commit()
+    except Exception as e:
+        print(str(e))
+create_table()
 
 #consumer key, consumer secret, access token, access secret.
 ckey=""
@@ -20,22 +29,35 @@ csecret=""
 atoken=""
 asecret=""
 
-
 class listener(StreamListener):
 
-
     def on_data(self, data):
-        print(data)
-        return (True)
+        try:
+            data = json.loads(data)
+            tweet = unidecode(data['text'])
+            time_ms = data['timestamp_ms']
+            vs = analyzer.polarity_scores(tweet)
+            sentiment = vs['compound']
+            print(time_ms, tweet, sentiment)
+            c.execute("INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)",
+                  (time_ms, tweet, sentiment))
+            conn.commit()
 
+        except KeyError as e:
+            print(str(e))
+        return(True)
 
     def on_error(self, status):
         print(status)
 
 
-auth = OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
+while True:
 
-
-twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["deeplearning"])
+    try:
+        auth = OAuthHandler(ckey, csecret)
+        auth.set_access_token(atoken, asecret)
+        twitterStream = Stream(auth, listener())
+        twitterStream.filter(track=["sports"])
+    except Exception as e:
+        print(str(e))
+        time.sleep(5)
